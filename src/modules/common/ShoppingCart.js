19c48f13fb70cfd4715bcad917e76f84afaad461
Sai Krishna Teja D <saikrishnatejad@gmail.com>
2021-04-27 13:16:07 -0500

    Persist orders and transactions

--- src/modules/common/ShoppingCart.js
+++ src/modules/common/ShoppingCart.js
@@ -3,13 +3,18 @@ import CloseButton from "react-bootstrap/CloseButton";
 import React, {useEffect, useState} from "react";
 import {serverURL} from "../../app.constants";
 import {isEmpty} from "../utils/util";
-
+import UserProfileStyles from "../styles/CommonStyles";
 
 function ShoppingCart(props) {
 
   const [rows, setRows] = useState([]);
   const [isStale, setIsStale] = useState(true);
   const [totalSum, setTotalSum] = useState(0);
+  const [checkout, setCheckout] = useState(false);
+  const [products, setProducts] = useState(null);
+  const [status, setStatus] = useState("");
+  const [paymentMethod, setPaymentMethod] = useState("Cash-On-Delivery");
+  const [addressForDelivery, setAddressForDelivery] = useState("")
 
   useEffect(async () => {
     if (isStale) {
@@ -23,6 +28,7 @@ function ShoppingCart(props) {
       });
       if (!isEmpty(tempData) && isEmpty(tempData.error)) {
         createItemList(tempData);
+        setProducts(tempData);
         url = `${serverURL}/shoppingCartTotal?userID=${encodeURIComponent(props.userID)}`;
         await fetch(url).then(response => response.json()).then((data) => {
           setTotalSum(data.totalSum);
@@ -30,12 +36,43 @@ function ShoppingCart(props) {
           console.log(error);
           alert(error + "\nLooks like the server is down, please try after sometime.");
         });
-      } else {
+      }
+      else {
         setTotalSum(0);
       }
       setIsStale(false);
     }
   }, [isStale]);
+
+  const getUserAddress = async () => {
+    let url = `${serverURL}/address?userID=${encodeURIComponent(props.userID)}&status='PRIMARY'`;
+    await fetch(url).then(response => response.json()).then((data) => {
+      console.log(data)
+      if (isEmpty(data.error)) {
+        setAddressForDelivery(
+          data.addr1 + "\n" + data.addr2 + "\n" + data.city + "\n" + data.stateInCountry + "\n" + data.country + "\n" + data.zipCode);
+      }
+      else {
+        alert("User address not found, please enter address for delivery")
+      }
+    }).catch((error) => {
+      console.log(error);
+      alert(error + "\nLooks like the server is down, please try after sometime.");
+    });
+  }
+
+  const buildOrderList = (orderID) => {
+    if (products == null) return [];
+    let orderList = []
+    let eachOrder = {}
+    products.map(product => {
+      eachOrder.orderID = orderID;
+      eachOrder.productID = product.productID;
+      orderList.push(eachOrder);
+      eachOrder = {}
+    });
+    return orderList;
+  }
 
   const createItemList = (data) => {
     setRows([]);
@@ -70,6 +107,21 @@ function ShoppingCart(props) {
     setIsStale(true);
   }
 
+  const deleteAllCartItems = async () => {
+    /*confirm("Are you sure about deleting the cart item ?");*/
+    setRows([])
+    let url = `${serverURL}/allShoppingCart?userID=${encodeURIComponent(props.userID)}`;
+    const requestOptionsForCartDelete = {
+      method: "DELETE",
+    };
+    await fetch(url, requestOptionsForCartDelete).then(response => response.json()).then((data) => {
+      // alert("cart item deleted successfully");
+    }).catch((error) => {
+      console.log(error);
+    });
+    setIsStale(true);
+  }
+
 
   const renderItem = (item, row) => {
     return (
@@ -81,19 +133,19 @@ function ShoppingCart(props) {
           {item.title}
         </div>
         <div className="col" style={{justifyContent: "center", width: 150, fontSize: 20}}>
-          {item.price+' $'}
+          {item.price + ' $'}
         </div>
         <div className="col" style={{justifyContent: "center"}}>
           <input type={"text"} onChange={(event) => updateCartQuantity(event.target.value, item)}
                  value={item.quantity} style={{fontSize: 20, border: 1, width: "100%"}} readOnly={false}/>
         </div>
         <div className="col" style={{justifyContent: "center"}}>
-          <input type={"text"} value={item.totalSum +' $'} style={{fontSize: 20, border: 1, width: "100%"}}
+          <input type={"text"} value={item.totalSum + ' $'} style={{fontSize: 20, border: 1, width: "100%"}}
                  readOnly={false}/>
         </div>
-        <div>
+        {!checkout && <div>
           <CloseButton onClick={() => deleteCartItem(item)}/>
-        </div>
+        </div>}
       </div>
     );
   }
@@ -119,20 +171,70 @@ function ShoppingCart(props) {
     setIsStale(true);
   }
 
-  async function placeOrder() {
-    console.log("placeOrder")
+  const placeOrder = async () => {
+    if (window.confirm("Orders once placed cannot be cancelled. Are you sure you want to continue ?")) {
+      let currTime = Date.now();
+      console.log(currTime)
+      console.log(props.userID)
+      let orderID = props.userID + currTime;
+      console.log(orderID)
+      let orderList = buildOrderList(orderID);
+      if (!isEmpty(orderList)) {
+        const requestOptions = {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(orderList)
+        };
+        await fetch(`${serverURL}/order`, requestOptions)
+          .then(response => response.json())
+          .then(async data => {
+            const requestOptions = {
+              method: 'POST',
+              headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({
+                orderID: data.orderID,
+                userID: props.userID,
+                paymentMethod: paymentMethod,
+                status: "Confirmed",
+                address: addressForDelivery,
+                totalAmount: totalSum,
+                dateOfTransaction: Date.now().toString(),
+              })
+            };
+            await fetch(`${serverURL}/transactions`, requestOptions)
+              .then(response => response.json())
+              .then(async (data) => {
+                alert("trx successful", data.TrxID);
+                //await updateInventory();
+                await deleteAllCartItems();
+                props.closeCart();
+              }).catch((error) => {
+              console.log(error)
+            })
+          })
+          .catch((error) => {
+            console.log(error)
+            alert(error);
+          });
+      }
+    }
+    else {
+      return;
+    }
+
   }
 
   return (
     <Modal show={true} onHide={() => props.closeCart()} size="lg">
       <Modal.Header>
-        <Modal.Title>Shopping Cart</Modal.Title>
+        {!checkout && <Modal.Title>Shopping Cart</Modal.Title>}
+        {checkout && <Modal.Title>Final Checkout Details</Modal.Title>}
         <CloseButton onClick={() => {
           props.closeCart();
         }}/>
       </Modal.Header>
       <Modal.Body>
-          {/*<div className={"row"}>
+        {/*<div className={"row"}>
             <div className="col" style={{justifyContent: "center", fontSize: 20, color: "orange"}}>
               {"Image"}
             </div>
@@ -149,20 +251,50 @@ function ShoppingCart(props) {
               {"Total"}
             </div>
           </div>*/}
-          {!isEmpty(rows) && <div style={{backgroundColor: "white"}} className="container">
-            {Object.keys(rows).map(row => {
-              return (
-                <div className="row" key={row} style={{padding: 10, alignItems: 'center'}}>
-                  {rows[row].map(item => {
-                    return (
-                      renderItem(item, row)
-                    )
-                  })}
+        {!isEmpty(rows) && <div style={{backgroundColor: "white"}} className="container">
+          {Object.keys(rows).map(row => {
+            return (
+              <div className="row" key={row} style={{padding: 10, alignItems: 'center'}}>
+                {rows[row].map(item => {
+                  return (
+                    renderItem(item, row)
+                  )
+                })}
+              </div>
+            );
+          })
+          }
+        </div>}
+        {checkout && (
+          <div>
+            <div className="col-md-6">
+              <div className="col-md-6">
+                <label>Payment method : </label>
+                <input placeholder={"Payment Method"} type="text" value={paymentMethod}
+                       onChange={(event) => setPaymentMethod(event.target.value)}
+                       style={UserProfileStyles.textBoxComp}
+                       required={true} readOnly={true}/>
+              </div>
+              <div className="col-md-6">
+                <div>
+                  <label>Delivery Address:</label>
                 </div>
-              );
-            })
-            }
-          </div>}
+                <div>
+              <textarea rows={6} placeholder={"Address for delivery"} type="text" value={addressForDelivery}
+                        onChange={(event) => setAddressForDelivery(event.target.value)} required={true}
+                        readOnly={false}/>
+                </div>
+              </div>
+              <div className={"col"} style={{paddingLeft: 80}}>
+                <input type="submit" onClick={async (event) => {
+                  event.preventDefault();
+                  await getUserAddress();
+                }} className="profile-edit-btn" name="btnAddMore"
+                       value="Get your address"/>
+              </div>
+            </div>
+          </div>
+        )}
       </Modal.Body>
       <Modal.Footer>
         <div>
@@ -171,11 +303,22 @@ function ShoppingCart(props) {
                  style={{
                    width: 200, justifyContent: "center", textAlign: "center", paddingRight: 10
                  }} required={true}/>
-          <input type="submit" onClick={async (event) => {
-            event.preventDefault();
+          {!checkout && <input type="submit" onClick={(event) => setCheckout(true)}
+                               className="profile-edit-btn"
+                               name="btnAddMore"
+                               value="Checkout and Pay" disabled={totalSum == 0}/>}
+          {checkout && <input type="submit" onClick={async (e) => {
+            e.preventDefault();
             await placeOrder();
-          }} className="profile-edit-btn" name="btnAddMore"
-                 value="Place Order" disabled={totalSum==0}/>
+          }}
+                              className="profile-edit-btn"
+                              name="btnAddMore"
+                              value="Place Order" disabled={totalSum == 0}/>}
+          {checkout && <input type="submit" onClick={(event) => setCheckout(false)}
+                              className="profile-edit-btn"
+                              name="btnAddMore"
+                              value="Cancel"/>}
+
         </div>
       </Modal.Footer>
     </Modal>
